1a96d3ea1f0498414956bd7980549f0efc500e3b
allanalarcon <samuel.alarcon@outlook.com>
2021-12-07 13:13:06 -0500

    First commit

---
+++ src/main/java/com/spring/di/SpringDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+	}
+
+}
